4acbe3e10bd967c2fb3c25465cf1f16f8f46df2b
Zauberfisch <admin@zauberfisch>
2013-03-09 15:32:58 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# mini SASS / SCSS & Compass talk
+## Salzburg Barcamp 2013
+
+all comments and notes in scss/screen.scss
---
+++ config.rb
@@ -0,0 +1,13 @@
+project_type = :stand_alone
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "scss"
+images_dir = "images"
+fonts_dir = "fonts"
+#output_style = :compressed
+output_style = :extended
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
